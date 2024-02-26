d933e598cd1828129d0f6bd3636b356feaa325a1
Guerizim <gueri_leon@outlook.com>
2024-02-26 08:16:50 -0800

    Added images to the reels

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 /*----- constants -----*/
+const symbols = ["A", "B", "C"]
 
 /*----- app's state (variables) -----*/
+let reels = Array.from({length: 3}, function(){
+  return getRandomSymbol
+})
 
 /*----- cached element references -----*/
+const reelsEl = document.getElementById("reels")
+const spinButtonEl = document.querySelector(".spin-button")
 
 /*----- event listeners -----*/
+spinButtonEl.addEventListener("click",function() {
+  shuffleReels()
+})
 
 /*----- functions -----*/
+init()
+
+function()
